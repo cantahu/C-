f780b3e07b10edca5c57260e45a86364a3bbcb8c
cantahu <cantahu@gmail.com>
2019-03-23 16:10:46 +0800

    Create jhstr.h

---
+++ DSAA/jhstr.h
@@ -0,0 +1,12 @@
+#ifndef _JHSTR_H_
+#define _JHSTR_H_
+
+int jhstr_digit_chk(char c);
+int jhstr_lowercase_chk(char c);
+int jhstr_uppercase_chk(char c);
+int jhstr_alpha_chk(char c);
+char jhstr_alpha_lower(char c);
+char jhstr_alpha_upper(char c);
+
+
+#endif /* _JHSTR_H_ */
